9fbd8fabaa330ca6ea06103fb2c92347566d2581
Artur <drugartur5@gmail.com>
2021-05-27 13:41:01 +0300

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {BrowserRouter} from "react-router-dom";
 
 const App = () => {
     return (<div>
-        <BrowserRouter>
+        <BrowserRouter basename="/hipstagram-useState">
             <Application/>
         </BrowserRouter>
     </div>);
